6cc9d865f7f95d546420ae78b74c9f44f74e00f5
justinnavarr0-ga <justinnavarr0@yahoo.com>
2023-04-19 00:44:14 -0700

    got the names to appear a lot nicer by using a grid

--- src/components/TripForm/TripForm.jsx
+++ src/components/TripForm/TripForm.jsx
@@ -39,14 +39,15 @@ return (
 						<div className="relative">
             <input type="text" name="name" value={newTrip} onChange={handleChange} required className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="" />							<label className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Trip Name</label>
 						</div>
-            
-    
+            <label>Choose Some Friends To Invite!</label>
+            <div className="grid grid-cols-3 gap-4">
             {allUsers.map((person) => 
-  <div key={person.id}>
-    <input id={person.name} type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-    <label>{person.name}</label>
-  </div>
-)}
+              <div key={person.id} className="flex items-center">
+                <input id={person.name} type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                <label htmlFor={person.name} className="ml-2">{person.name}</label>
+              </div>
+              )}
+            </div>
 						<div className="relative">
 							<button type='submit' className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
 						</div>
